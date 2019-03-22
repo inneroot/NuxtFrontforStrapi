4cdc3339d199a1d01d93d990bc31865f9544ad7e
Gleb Fedotov <innercoding@gmail.com>
2019-03-22 18:41:37 +0300

    Hooper add

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="maincontainer">
     <header>{{slides}}</header>
+    <MainSlider/>
     <main role="main">{{posts}}</main>
   </div>
 </template>
 
 <script>
 import Strapi from 'strapi-sdk-javascript/build/main'
+import MainSlider from '~/components/MainSlider.vue'
+
 const apiUrl = process.env.API_URL || 'http://localhost:1337'
 const strapi = new Strapi(apiUrl)
 
 export default {
+  components: {
+    MainSlider
+  },
   data() {
     return {}
   },
